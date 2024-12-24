1888fd7a2b283034bb06b6030871e4867d0d26fb
Alex <alexandrepinto586@gmail.com>
2024-12-24 10:06:16 -0300

    Atualização do README, main.py, notebook e do requirements

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Python 3.12
 ## Instalação das bibliotecas:
 
 ```
+python -m venv venv
+venv/Scripts/Activate
 pip install -r requirements.txt
 ```
--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import numpy as np
 
 class Analise:
     def __init__(self, data: str):
-        self.data = data
+        self.data = pd.read_csv(data, sep=';')
+
+    def calcular_valores(self):
+        pass
+
+    def generate_graphs(self):
+        pass
 
     def generate_report(self):
         pass
